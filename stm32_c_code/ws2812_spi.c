f5aac27114be052cf18ef2ed2badd22212eaf715
Ellu <lutaoyu518@gmail.com>
2023-12-28 13:36:23 +0800

    mod c code

--- stm32_c_code/ws2812_spi.c
+++ stm32_c_code/ws2812_spi.c
@@ -10,8 +10,6 @@
 
 #include "ws2812_spi.h"
 
-#if __has_include("spi.h")
-#include "log.h"
 #include "spi.h"
 #include "stdio.h"
 #include "stdlib.h"
@@ -29,7 +27,7 @@
 #define BIT0 0b100
 #define BUF_LEN(n) ((n) * BIT_LEN * 3)  // n个灯需要的缓冲区长度 / bytes
 
-// #define WAIT_TIME 50  // 发送间隔 / us
+#define IS_GRB_FORMAT 1  // 0: RGB 1: GRB
 
 // SPI空闲时MOSI总线为高电平, 这可能造成第一个数据发送错误
 // 首尾灯珠不正常时, 在数据前后添加一定数量的低电平或添加一个额外的灯珠,
@@ -41,12 +39,11 @@ HAL_StatusTypeDef Strip_Init(LEDStrip_t *strip, uint16_t length,
                              SPI_HandleTypeDef *hspi) {
   if (hspi == NULL) hspi = strip->hspi;
   if (strip->buffer != NULL || strip->length != 0) {
-    LOG_W("LED REINIT");  // 最好手动调用Strip_DeInit再重新初始化
     Strip_DeInit(strip);
   }
   strip->buffer = malloc(BUF_LEN(length) + HEAD_ZERO + TAIL_ZERO);
   if (strip->buffer == NULL) {
-    LOG_E("LED MALLOC FAILED");
+    // LOG_E("LED MALLOC FAILED");
     return HAL_ERROR;
   }
   strip->length = length;
@@ -65,14 +62,16 @@ void Strip_DeInit(LEDStrip_t *strip) {
   strip->length = 0;
 }
 
-void Strip_Set(LEDStrip_t *strip, uint16_t index, uint32_t RGBcolor) {
+void Strip_Set(LEDStrip_t *strip, uint16_t index, uint32_t color) {
   if (index >= strip->length || (!strip->buffer)) return;  // overrun check
   uint8_t *buf = strip->buffer + index * BIT_LEN * 3 + HEAD_ZERO;
-  uint32_t GRBdata = (RGBcolor & 0x00FF00) << 8 | (RGBcolor & 0xFF0000) >> 8 |
-                     (RGBcolor & 0x0000FF);
+#if IS_GRB_FORMAT
+  color =
+      (color & 0x00FF00) << 8 | (color & 0xFF0000) >> 8 | (color & 0x0000FF);
+#endif
   memset(buf, 0, BIT_LEN * 3);
   for (uint16_t bit_offset = 0; bit_offset < BIT_LEN * 8 * 3; bit_offset++) {
-    if (GRBdata & (((uint32_t)1) << (23 - bit_offset / BIT_LEN))) {
+    if (color & (((uint32_t)1) << (23 - bit_offset / BIT_LEN))) {
       buf[bit_offset / 8] |=
           ((BIT1 >> (BIT_LEN - 1 - bit_offset % BIT_LEN)) & 0x01)
           << (7 - bit_offset % 8);
@@ -84,29 +83,12 @@ void Strip_Set(LEDStrip_t *strip, uint16_t index, uint32_t RGBcolor) {
   }
 }
 
-void Strip_Set_RGB(LEDStrip_t *strip, uint16_t index, uint32_t RGBcolor) {
-  if (index >= strip->length || (!strip->buffer)) return;  // overrun check
-  uint8_t *buf = strip->buffer + index * BIT_LEN * 3 + HEAD_ZERO;
-  uint32_t GRBdata = RGBcolor;
-  memset(buf, 0, BIT_LEN * 3);
-  for (uint16_t bit_offset = 0; bit_offset < BIT_LEN * 8 * 3; bit_offset++) {
-    if (GRBdata & (((uint32_t)1) << (23 - bit_offset / BIT_LEN))) {
-      buf[bit_offset / 8] |=
-          ((BIT1 >> (BIT_LEN - 1 - bit_offset % BIT_LEN)) & 0x01)
-          << (7 - bit_offset % 8);
-    } else {
-      buf[bit_offset / 8] |=
-          (((BIT0 >> (BIT_LEN - 1 - bit_offset % BIT_LEN)) & 0x01)
-           << (7 - bit_offset % 8));
-    }
-  }
-}
 void Strip_Set_Range(LEDStrip_t *strip, uint16_t start, uint16_t end,
-                     uint32_t RGBcolor) {
+                     uint32_t color) {
   if (!strip->length) return;
   if (end >= strip->length) end = strip->length - 1;
   for (uint16_t i = start; i <= end; i++) {
-    Strip_Set(strip, i, RGBcolor);
+    Strip_Set(strip, i, color);
   }
 }
 
@@ -206,5 +188,3 @@ uint32_t HSV_To_RGB(float h, uint8_t s, uint8_t v) {
 
   return ((uint32_t)r << 16) | ((uint32_t)g << 8) | b;
 }
-
-#endif  // __has_include("spi.h")
